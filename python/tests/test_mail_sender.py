7058b84dcdfa4ecfc7481a8c1d3bc128b9cf8cc8
flolgl <florianlegal@outlook.fr>
2024-11-26 15:33:47 +0100

    Added handmade mocks for test_mail_sender.py to fail

--- python/tests/test_mail_sender.py
+++ python/tests/test_mail_sender.py
@@ -1,8 +1,48 @@
-def test_send_v1():
+from dataclasses import dataclass
+
+import pytest
+
+from mail_sender import SendMailRequest, MailSender, SendMailResponse
+from user import User
+
+
+@dataclass
+class SendFullMailResponse:
+    code: int
+    message: str
+    request: SendMailRequest
+
+
+class HttpClient:
+    def __init__(self):
+        self._request = None
+
+    def post(self, url: str, request: SendMailRequest) -> SendFullMailResponse:
+        return SendFullMailResponse(503, request.recipient, request)
+
+
+@pytest.fixture
+def http_client() -> HttpClient:
+    return HttpClient()
+
+
+@pytest.fixture
+def user() -> User:
+    return User("User", "user@email.fr")
+
+
+def test_send_v1(http_client: HttpClient, user: User):
     # TODO: write a test that fails due to the bug in MailSender.send_v1
-    pass
+    mail_sender: MailSender = MailSender(http_client)
+
+    res = mail_sender.send_v1(user, "Message")
+
+    assert res.request.recipient == user.email
+    assert res.request.subject == "New notification"
 
 
-def test_send_v2():
+def test_send_v2(http_client: HttpClient, user: User):
     # TODO: write a test that fails due to the bug in MailSender.send_v2
-    pass
+    mail_sender: MailSender = MailSender(http_client)
+
+    res = mail_sender.send_v2(user, "Message")
