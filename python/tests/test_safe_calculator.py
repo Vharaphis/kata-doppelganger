f3be1b0463197f430b3fa8297525e7b3dcb46daf
flolgl <florianlegal@outlook.fr>
2024-11-26 18:32:47 +0100

    Fixed authorizer_mock method not being a fixture

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,9 +1,11 @@
 from unittest.mock import Mock
 
+import pytest
+
 from safe_calculator import SafeCalculator
 
-
-def authorizer():
+@pytest.fixture
+def authorizer_mock():
     authorizer_mock = Mock()
     authorizer_mock.authorize.return_value = True
     return authorizer_mock
@@ -12,4 +14,4 @@ def test_divide_should_not_raise_any_error_when_authorized(authorizer_mock):
     # TODO: write a test that fails due to the bug in
     # SafeCalculator.add
     calc: SafeCalculator = SafeCalculator(authorizer_mock)
-    calc.add(1, 1)  # Should raise an exception because Authorize returns False
+    calc.add(1, 1)  # Should raise an exception because Authorize returns True
