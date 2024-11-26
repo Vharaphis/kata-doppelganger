05c16a2385107d37db6c25657754c332089194de
flolgl <florianlegal@outlook.fr>
2024-11-26 15:18:45 +0100

    Added handmade mocks for test_safe_calculator.py

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,7 +1,17 @@
-import pytest
+from safe_calculator import SafeCalculator
 
 
 def test_divide_should_not_raise_any_error_when_authorized():
     # TODO: write a test that fails due to the bug in
     # SafeCalculator.add
-    pass
+    authorizer: Authorizer = Authorizer()
+    calc: SafeCalculator = SafeCalculator(authorizer)
+    calc.add(1, 1)  # Should raise an exception because Authorize returns False
+
+
+class Authorizer:
+    def __init__(self):
+        pass
+
+    def authorize(self):
+        return True
