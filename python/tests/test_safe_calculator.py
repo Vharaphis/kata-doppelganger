a0ede0841f62ad7b0556a218ab87b027258a1220
flolgl <florianlegal@outlook.fr>
2024-11-26 15:47:55 +0100

    Added unittest mocks for test_safe_calculator.py

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,17 +1,15 @@
+from unittest.mock import Mock
+
 from safe_calculator import SafeCalculator
 
 
-def test_divide_should_not_raise_any_error_when_authorized():
+def authorizer():
+    authorizer_mock = Mock()
+    authorizer_mock.authorize.return_value = True
+    return authorizer_mock
+
+def test_divide_should_not_raise_any_error_when_authorized(authorizer_mock):
     # TODO: write a test that fails due to the bug in
     # SafeCalculator.add
-    authorizer: Authorizer = Authorizer()
-    calc: SafeCalculator = SafeCalculator(authorizer)
+    calc: SafeCalculator = SafeCalculator(authorizer_mock)
     calc.add(1, 1)  # Should raise an exception because Authorize returns False
-
-
-class Authorizer:
-    def __init__(self):
-        pass
-
-    def authorize(self):
-        return True
